bd1bc479dc55d6c6f3510e3851182791dc93a629
nulltek <steve.public@nulltek.xyz>
2022-10-30 15:24:07 +1300

    Migrating to the new UOS Protocol.

--- API.ino
+++ API.ino
@@ -12,33 +12,32 @@
 // executes the pending instruction and updates system memory where required
 bool handle_comms() {
   switch (instruction_address) {
-    case 64:
-      // raw digital IO override.
-      if ((instruction_len % 3) != 0) {
-        return false;  // the payload is wrong for this instruction type
-      }
-      // Execute each instruction received.
-      for (int i = 0; i < instruction_len / 3; i++) {
-        if (instruction_payload[uint8_t(i * 3 + 1)] == 0) {
-          // DIO Output
-          switch (instruction_payload[uint8_t(i * 3 + 2)]) {
-            // Check the level byte
-            case 0:  // set low
-              if (!write_io(instruction_payload[uint8_t(i * 3)],
-                            GPIO_OUTPUT_LOW))
-                return false;
-              break;
-            case 1:  // set high
-              if (!write_io(instruction_payload[uint8_t(i * 3)],
-                            GPIO_OUTPUT_HIGH))
-                return false;
-              break;
-            default:
-              return false;  // instruction not formed correctly
-          }
-        }
-      }
-      return true;  // all actions completed successfully
+    case 60:
+      // Set GPIO Output non-volatile.
+      return set_gpio_outputs();
+  }
+  return false;  // Unsupported instruction.
+}
+
+// Executes instructions for setting GPIO outputs.
+bool set_gpio_outputs() {  // 60
+  // Check the payload is correct length.
+  if ((instruction_len % 2) != 0) return false;
+  // Execute each instruction received.
+  for (int i = 0; i < instruction_len / 2; i++) {
+    switch (instruction_payload[uint8_t(i * 2 + 1)]) {
+      // Check the level byte
+      case 0:  // set low
+        if (!write_io(instruction_payload[uint8_t(i * 2)], GPIO_OUTPUT_LOW))
+          return false;
+        break;
+      case 1:  // set high
+        if (!write_io(instruction_payload[uint8_t(i * 2)], GPIO_OUTPUT_HIGH))
+          return false;
+        break;
+      default:
+        return false;  // instruction not formed correctly
+    }
   }
   return false;
 }
--- CHANGES.rst
+++ CHANGES.rst
@@ -3,9 +3,10 @@ Version 0.6.0
 
 :Date: TBC
 
+* Updating for redesigned UOS protocol.
 * Redesigned the supported device hardware to be defined in a more concise manner.
 * Redesigned IO handling to reduce wasted memory.
 * Removing 16-bit types being used for loop variables.
 * Ensuring pins are defined before trying to use them.
-* Remove non-binary search array lookup as all our definitiuon arrays will be sorted.
+* Remove non-binary search array lookup as all our definition arrays will be sorted.
 * Fixing the arduino uno definition. This device has fewer ADC pins than the nano.
